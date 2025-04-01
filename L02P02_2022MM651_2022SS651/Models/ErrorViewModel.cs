ee600bb5ab666c638c8fd1e7412d406a243e9066
Angel Magana <maganaangel342@gmail.com>
2025-04-01 17:08:36 -0600

    first commit

---
+++ L02P02_2022MM651_2022SS651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022MM651_2022SS651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
